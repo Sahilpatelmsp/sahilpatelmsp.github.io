4f08f64692d27ec47054e3922ca64a3a5306277c
Sahilpatelmsp <91902356+Sahilpatelmsp@users.noreply.github.com>
2021-11-11 19:56:20 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php	
+$firstName = $_POST['firstName'];	
+$lastName = $_POST['lastName'];	
+$gender = $_POST['gender'];
+	$email = $_POST['email'];	
+	$password = $_POST['password'];
+	$number = $_POST['number'];
+
+$conn= ne2 mysql i()
+$sql = "SELECT * FROM `Registration`";
+	?>
+	
